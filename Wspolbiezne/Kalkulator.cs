39cecd4aebc711040198ca8a47cee843064755b4
tomczykp <tomczykp987@gmail.com>
2022-03-24 17:37:02 +0100

    Dodanie projektu bazowego

---
+++ Wspolbiezne/Kalkulator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wspolbiezne
+{
+    public class Kalkulator 
+    {
+        private int liczba;
+
+        public Kalkulator(int a) 
+        {
+            this.liczba = a;
+        }
+
+        public int Add(int b)
+        {
+            return this.liczba + b;
+        }
+
+        public int Substract(int b)
+        {
+            return this.liczba - b;
+        }
+
+        public int Mul(int b)
+        {
+            return this.liczba * b;
+        }
+
+        public int Div(int b)
+        {
+            if (b == 0)
+                throw new ArithmeticException();
+            return this.liczba / b;
+        }
+
+    }
+}
---
+++ Wspolbiezne/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Wspolbiezne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kalkulator kalkulator = new(10);
+
+            System.Console.WriteLine(kalkulator.Add(5));
+        }
+    }
+}
